84bc6030dd9e7a28bb3c2e42dbc6d05d7ca0bebc
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-30 18:46:59 +0200

    Add space

--- wac.cmd
+++ wac.cmd
@@ -3,6 +3,6 @@ setlocal
 set /p login="Enter username: "
 set /p pass="Enter password: "
 net user %login% %pass% /add
-set /p admin="Is admin? [Y]/[N]"
+set /p admin="Is admin? [Y]/[N] "
 if "%admin%"=="y" net localgroup Administrators %login% /add
 exit /b 0
