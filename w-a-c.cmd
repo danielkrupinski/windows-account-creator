9150bae7aa050266e5982eec37f7c98f04e55876
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-30 17:51:26 +0200

    Add account creator

---
+++ w-a-c.cmd
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+set /p login="Enter username: "
+set /p pass="Enter password: "
+net user %login% %pass% /add
+set /p admin="Is admin? [Y]/[N]"
+if %admin%=="y" net local group Administrators %login% /add
+exit /b 0
