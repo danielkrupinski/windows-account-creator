60bc981fb92a6e380e47d771d77952e3c3ea0e65
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-30 18:40:13 +0200

    Fix if statement

--- wac.cmd
+++ wac.cmd
@@ -4,5 +4,5 @@ set /p login="Enter username: "
 set /p pass="Enter password: "
 net user %login% %pass% /add
 set /p admin="Is admin? [Y]/[N]"
-if %admin%=="y" net localgroup Administrators %login% /add
+if "%admin%"=="y" net localgroup Administrators %login% /add
 exit /b 0
