4d3e6a9842b171fd4133bbe212cbd6616ab4b992
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-04 19:25:50 +0200

    Hide output of net user %login% %pass% /add

--- wac.cmd
+++ wac.cmd
@@ -2,7 +2,7 @@
 setlocal
 set /p login="Enter username: "
 set /p pass="Enter password: "
-net user %login% %pass% /add
+net user %login% %pass% /add > nul
 set /p admin="Is admin (Y/N)? "
 if "%admin%"=="y" net localgroup Administrators %login% /add
 exit /b 0
