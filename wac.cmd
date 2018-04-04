be9691f836eeac02a59280d2b4b9ed9cbdfd2cf9
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-04 19:33:40 +0200

    Add error handling

--- wac.cmd
+++ wac.cmd
@@ -2,8 +2,11 @@
 setlocal
 set /p login="Enter username: "
 set /p pass="Enter password: "
-net user %login% %pass% /add > nul
+net user %login% %pass% /add > nul || goto :not_an_admin
 set /p admin="Is admin (Y/N)? "
 if "%admin%"=="y" net localgroup Administrators %login% /add > nul
 echo Success!
 exit /b 0
+
+:not_an_admin
+exit /b 1
