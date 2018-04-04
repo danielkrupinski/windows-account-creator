1e34d05aa40740cf05942e31285c11c5ac85a468
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-04 19:41:32 +0200

    Update formatting

--- wac.cmd
+++ wac.cmd
@@ -1,10 +1,14 @@
 @echo off
 setlocal
+
 set /p login="Enter username: "
 set /p pass="Enter password: "
+
 net user %login% %pass% /add > nul || goto :not_an_admin
+
 set /p admin="Is admin (Y/N)? "
 if "%admin%"=="y" net localgroup Administrators %login% /add > nul || goto :group_not_found
+
 echo Success!
 exit /b 0
 
