6f8481eee6b9c35570957bfe723d9e7ef51ee4a9
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-04 19:40:40 +0200

    Update error handling

--- wac.cmd
+++ wac.cmd
@@ -4,9 +4,13 @@ set /p login="Enter username: "
 set /p pass="Enter password: "
 net user %login% %pass% /add > nul || goto :not_an_admin
 set /p admin="Is admin (Y/N)? "
-if "%admin%"=="y" net localgroup Administrators %login% /add > nul
+if "%admin%"=="y" net localgroup Administrators %login% /add > nul || goto :group_not_found
 echo Success!
 exit /b 0
 
 :not_an_admin
 exit /b 1
+
+:group_not_found
+net user %login% /delete > nul
+exit /b 2
